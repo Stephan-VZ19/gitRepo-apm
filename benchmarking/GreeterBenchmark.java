48e50f1d4e8211cb89b5a1915fe0f53749a1af76
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2024-02-27 13:25:02 +0100

    update

--- benchmarking/GreeterBenchmark.java
+++ benchmarking/GreeterBenchmark.java
@@ -1,19 +1,25 @@
 package ch.fhnw.apm.hello;
 
 import org.openjdk.jmh.Main;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.*;
 
 import java.io.IOException;
 
+@State(Scope.Benchmark)
 public class GreeterBenchmark {
     // TODO
 
+    Greeter greeter;
+    @Setup
+    public void setup() {
+        greeter = new Greeter(100);
+    }
+
     @Benchmark
     @Fork(1)
+    @Warmup(iterations = 5, time = 2)
+    @Measurement(iterations = 2, time = 2)
     public void generateGreeting() {
-
-        var greeter = new Greeter(100);
         greeter.generateGreeting();
     }
 
